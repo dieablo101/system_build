0c1def104da405ace8203c4e345f16bacda807f5
dieablo101 <kidlucrid@gmail.com>
2026-01-06 21:46:16 +0000

    not sure where im at, just exploring

--- editor_build.sh
+++ editor_build.sh
@@ -1,6 +1,20 @@
-!#/bash/bin
+
+#!/bin/sh
 
 # install ranger and emacs
-apk add emacs
-apk add ranger
+#apk add emacs
+#apk add ranger
 
+if ! command -v emacs > ~/../dev/null 2>&1; then
+    apk add emacs
+else
+    echo "Emacs already installed\n"
+fi
+
+if ! command -v ranger > ~/../dev/null 2>&1; then
+   apk add ranger
+else
+    echo "Ranger already installed\n\n"
+fi
+
+echo "editor builder running"
--- system.sh
+++ system.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sh ~/../sys/git_installer.sh
+sh ~/../sys/editor_build.sh
 
 # Create systemWide git to manage dev environment build / maintanance in ish IPAD IOS
